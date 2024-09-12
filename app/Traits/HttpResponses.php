cd5ec3e89bbbb30099896080c69bc1fbb6ee5c7b
Mahmod Mashlah <mashlahmahmod@gmail.com>
2024-09-12 13:44:09 +0300

    3- add HttpResponses.php Trait in app/Traits Folder

---
+++ app/Traits/HttpResponses.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Traits;
+
+trait HttpResponses
+{
+
+    protected function success($data, $message = null, $code = 200)
+    {
+        return response()->json([
+
+            'status-code' => $code,
+            'status' => 'Request was successful',
+            'message' => $message,
+            'data' => $data,
+
+        ], $code);
+    }
+
+    protected function error($data, $message = null, $code)
+    {
+        return response()->json([
+
+            'status-code' => $code,
+            'status' => ' Error has occoured ! ',
+            'message' => $message,
+            'data' => $data,
+
+        ], $code);
+    }
+}
